277c18dc90906621ee128741b18a620f09f03d08
Damian Tapia <damian.tapia45@gmail.com>
2026-04-08 00:22:25 -0600

    feat: add TextLink atom

---
+++ src/components/atoms/TextLink/index.tsx
@@ -0,0 +1,16 @@
+interface TextLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+export function TextLink({ href, children }: TextLinkProps) {
+  return (
+    <a
+      href={href}
+      className="font-semibold text-[#5c8fff] underline"
+      style={{ letterSpacing: "-0.32px" }}
+    >
+      {children}
+    </a>
+  );
+}
