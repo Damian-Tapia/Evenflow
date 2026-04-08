20af3c6d363fe834fd0a0885be19d294572f48a1
Damian Tapia <damian.tapia45@gmail.com>
2026-04-08 00:27:34 -0600

    feat: add AuthLayout template

---
+++ src/components/templates/AuthLayout/index.tsx
@@ -0,0 +1,15 @@
+interface AuthLayoutProps {
+  brandingPanel: React.ReactNode;
+  children: React.ReactNode;
+}
+
+export function AuthLayout({ brandingPanel, children }: AuthLayoutProps) {
+  return (
+    <div className="flex flex-col lg:flex-row h-screen">
+      {brandingPanel}
+      <section className="flex flex-col justify-center items-center px-8 py-10 lg:p-16 lg:w-1/2 bg-[#efefef] h-screen">
+        <div className="w-full max-w-[592px]">{children}</div>
+      </section>
+    </div>
+  );
+}
